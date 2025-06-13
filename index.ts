016ceaa85ef539055f9b737af5d3894a0cd6405b
gary.y.7811@gmail.com <gary.y.7811@gmail.com>
2025-06-13 19:44:10 -0400

    +

--- index.ts
+++ index.ts
@@ -5,4 +5,5 @@ export { DocumentValidationEventProducer } from '@odmd-rag/contracts-lib-rag';
 export { RagDocumentIngestionStack } from './lib/rag-document-ingestion-stack';
 export { RagDocumentIngestionAuthStack } from './lib/rag-document-ingestion-auth-stack';
 export { RagDocumentIngestionWebHostingStack } from './lib/rag-document-ingestion-web-hosting-stack';
-export { RagDocumentIngestionWebUIStack } from './lib/rag-document-ingestion-web-ui-stack'; 
+export { RagDocumentIngestionWebUIStack } from './lib/rag-document-ingestion-web-ui-stack';
+// trigger
